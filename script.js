0cc5cf4ddd8a624fd36e0a38136487ab6a733238
Bishop <mhabig1045@gmail.com>
2024-02-02 23:13:37 +0700

    Update

---
+++ script.js
@@ -0,0 +1,68 @@
+//console.log('Phanuphon Lachantha')
+
+/*
+let x = 2
+y = 10
+z = x + y
+console.log(z);
+*/
+
+/*
+//Operators
+a1 = 3 + 2 // 5
+a2 = 3 - 2 // 1
+a3 = 3 * 2 // 6
+a4 = 3 / 2 // 1.5
+a5 = 3 ** 2 // 9
+a6 = 3 % 2 // 1
+
+x = 5
+x ++  // 6
+x -- // 4
+x += 5 //10
+*/
+
+/*
+//ตัวเปรียบเทียบ
+let x = 7
+let y = 5
+
+let a1 = x > y //true
+let a2 = x >= y //true
+
+let a3 = x < y //false
+let a4 = x <= y //false
+
+let a5 = x === y //false
+let a6 = x !== y //true
+*/
+
+/*
+// ตรรกศาสตร์
+let p = true
+let q = false
+
+let a1 = p && q // false
+let a2 = p || q // true
+let a3 = !p     //false
+*/
+
+/*
+//conditions เงื่อนไข
+let score = 60
+if(score >= 80){
+    console.log('Grade A')
+}else if (score >= 70) {
+    console.log('Grade B')
+}else if (score >= 60){
+    console.log('Grade D')
+}else{
+    console.log('Grade F')
+}
+*/
+
+
+//Loop
+for (){
+    
+}
